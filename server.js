8515de8ee3b69541eaad060a0db79b49e7741dc8
MarcoZizzi-ScaiConsulting <70946036+MarcoZizzi-ScaiConsulting@users.noreply.github.com>
2020-09-08 12:09:42 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ var methodOverride = require('method-override');
 var redis = require('redis')
 
 // Configuration
-mongoose.connect(database.remoteUrl);                         // connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
-app.use(express.static(paths.staticFiles));                   // set the static files location /public/img will be /img for users
-app.use(morgan('dev'));                                       // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'}));         // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                   // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override'));            // override with the X-HTTP-Method-Override header in the request
+#mongoose.connect(database.remoteUrl);                         // connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+#app.use(express.static(paths.staticFiles));                   // set the static files location /public/img will be /img for users
+#app.use(morgan('dev'));                                       // log every request to the console
+#app.use(bodyParser.urlencoded({'extended': 'true'}));         // parse application/x-www-form-urlencoded
+#app.use(bodyParser.json());                                   // parse application/json
+#app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+#app.use(methodOverride('X-HTTP-Method-Override'));            // override with the X-HTTP-Method-Override header in the request
 
 // Routes
-require('./app/routes.js')(app);
+#require('./app/routes.js')(app);
 
 // Start listening
 app.listen(port);
@@ -35,6 +35,8 @@ client.on("error", function(error) {
 });
 
 client.set("key", "value", redis.print);
-var value = client.get("key", redis.print);
 
-console.log("Get key redis: " + value)
+client.get("key", function(err, reply) {
+  // reply is null when the key is missing
+  console.log("Get key redis: " + reply);
+});
